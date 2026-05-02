b6269a2a42c9c2d2e714b983bb44fc77994425ec
Muhammad Adnan <engr.adeelkhan2002@gmail.com>
2026-05-02 16:49:53 +0500

    fix urls

--- facebook/posting_blogs/urls.py
+++ facebook/posting_blogs/urls.py
@@ -4,11 +4,11 @@ from .views import *
 urlpatterns = [
     path('create-blog/',Blog_Create_view.as_view(),name= 'create-blog-view'),
     path('get-blog/<int:id>/',Blog_Get_view.as_view(),name= 'get-blog-view'),
-    path('user/get-all-blogs/<int:user_id>/',GetAllUserBlogsView.as_view(),name= 'get-all-user-blog-view'),
+    path('user/get-all-blogs',GetAllUserBlogsView.as_view(),name= 'get-all-user-blog-view'),
     path('edit-blog/<int:id>/',Blog_Patch_view.as_view(),name= 'put-blog-view'),
     path('delete-blog/<int:id>/',DeleteBlogView.as_view(),name =' Delete-view'),
     path('comment-blog/<int:blog_id>/',Comment_Create_View.as_view(),name= 'create-comment-view'),
-    path('user/get-all-comments/<int:user_id>/',GetAllUserCommentsView.as_view(),name= 'get-all-user-comments-view'),
+    path('user/get-all-comments/',GetAllUserCommentsView.as_view(),name= 'get-all-user-comments-view'),
     path('get-comment-of-blog/<int:id>/',Comment_Get_view.as_view(),name= 'get-comment-view'),
     path('edit-comment/<int:id>/',Comment_Put_view.as_view(),name= 'put-comment-view'),
     path('delete-comment/<int:id>/',Comment_Delete_view.as_view(),name =' Delete-comment-view'),
