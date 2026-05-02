0ff51aecdc3a56a7f5a943847345c1870af036eb
Muhammad Adnan <engr.adeelkhan2002@gmail.com>
2026-05-02 16:43:48 +0500

    fix the admin panel

--- facebook/posting_blogs/admin.py
+++ facebook/posting_blogs/admin.py
@@ -3,7 +3,15 @@ from .models import *
 # Register your models here.
 @admin.register(Comment)
 class CommentBlog(admin.ModelAdmin):
-    list_display = ['id','user','text','created_at','blog_text','blog_owner']
+    list_display = ['id','user','text','blog_text','blog_owner','created_at']
+    search_fields = ['user']
+    list_filter = ['created_at']
+
+    fieldsets = (
+        ('Comments', {'fields':('user','text','blog')}),
+    )
+
+    readonly_fields = ['blog_text','blog_owner']
 
     def blog_text(self,obj):
         return obj.blog.text
@@ -17,8 +25,15 @@ class CommentBlog(admin.ModelAdmin):
 
 @admin.register(Blogs)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['id','user', 'text', 'get_likes_count']
+    list_display = ['id','user', 'text','images','file' ,'get_likes_count','created_at']
+    search_fields = ['user']
+    list_filter = ['created_at']
+    
 
+    fieldsets = (
+        ('Blogs', {'fields':('user','text','images','file','get_likes_count')}),
+    )
+    readonly_fields = ['get_likes_count']
 
 
     def get_likes_count(self, obj):
@@ -28,17 +43,26 @@ class BlogAdmin(admin.ModelAdmin):
 
 @admin.register(Notification)
 class notify(admin.ModelAdmin):
-    list_display = ['id','user','message','is_read']
+    list_display = ['id','user','message','is_read','timestamp']
+    search_fields = ['user','is_read']
 
 
 @admin.register(Followers)
 class followers(admin.ModelAdmin):
-    list_display = ['id','user','followed_user','created_at']
+    list_display = ['id','user','followed_user','followers_counts','created_at']
+    search_fields = ['user']
+    ordering = ['followed_user']
+
+    def followers_counts(self,obj):
+        return Followers.objects.filter(followed_user = obj.followed_user, status = 'accepted').count()
+    
+    followers_counts.short_description = 'Followers Count'
 
 
 @admin.register(Story)
 class story(admin.ModelAdmin):
     list_display = ['id','user','media','caption','visibilty','viewers_count','expire_at','created_at']
+    search_fields = ['user']
 
 
     def viewers_count(self, obj):
