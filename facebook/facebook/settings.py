c92ef32eb979145594be94d660954769d6f1fd7f
Muhammad Adnan <engr.adeelkhan2002@gmail.com>
2026-05-02 16:39:06 +0500

    add postgresql

--- facebook/facebook/settings.py
+++ facebook/facebook/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'corsheaders',
     'daphne',
     'django.contrib.admin',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'adrf',
     'drf_yasg',
     'multiupload',
     'user',
@@ -113,24 +115,22 @@ CELERY_BEAT_SCHEDULE = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# DATABASES = {  
-#     'default': {  
-#         'ENGINE': 'django.db.backends.mysql',  
-#         'NAME': os.environ.get('DB_NAME'),  
-#         'USER':os.environ.get('DB_USER'),  
-#         'PASSWORD':os.environ.get('DB_PASSWORD'),  
-#         'HOST': os.environ.get('DB_HOST'),  
-#         'PORT': '3306',  
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }  
-# }  }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': os.environ.get('DB_NAME'),  
+        'USER':os.environ.get('DB_USER'),  
+        'PASSWORD':os.environ.get('DB_PASSWORD'),  
+        'HOST': os.environ.get('DB_HOST'),  
+        'PORT': '5432',  
+
+}  }
 
 # CHANNEL_LAYERS = {
 #     'default': {
@@ -276,4 +276,20 @@ LOGGING = {
             'propagate': False,
         },
     },
+}
+
+
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {                         
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'PERSIST_AUTH': True,
+    'JSON_EDITOR': True
 }
