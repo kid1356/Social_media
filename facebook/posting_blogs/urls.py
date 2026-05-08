037d450c033054ed565648bf5b15c4ec10d5734f
Muhammad Adnan <engr.adeelkhan2002@gmail.com>
2026-05-08 15:34:45 +0500

    making abstraction readable, add new endpoints and set naming on old endpoints

--- facebook/posting_blogs/urls.py
+++ facebook/posting_blogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import Blog_Create_view,Blog_Get_view,Blog_Patch_view,GetAllUserBlogsView,DeleteBlogView,Comment_Create_View,GetAllUserCommentsView,Comment_Get_view,Comment_Delete_view,Comment_Put_view,Like_Blog_view,User_Follow_View,GetFollowersView,GetFollowPendingRequestView,AcceptFollowView,RejectFollowView,UnfollowView,FollowingView,CreateStory,GetStory,TrackViewers,DeleteStory
 
 urlpatterns = [
     path('create-blog/',Blog_Create_view.as_view(),name= 'create-blog-view'),
@@ -13,10 +13,11 @@ urlpatterns = [
     path('edit-comment/<int:id>/',Comment_Put_view.as_view(),name= 'put-comment-view'),
     path('delete-comment/<int:id>/',Comment_Delete_view.as_view(),name =' Delete-comment-view'),
     path('like-blog/<int:id>/',Like_Blog_view.as_view(),name =' Like-view'),
-    path('follow/<int:id>/',User_Follow_View.as_view(),name =' followView'),
+    path('follow/<int:user_id>/',User_Follow_View.as_view(),name =' followView'),
+    path('get-pending-follow-requests/',GetFollowPendingRequestView.as_view(), name ="pendingrequests" ),
     path('accept-follow-request/<int:follow_request_id>/',AcceptFollowView.as_view(),name =' acceptfollowView'),
     path('reject-follow-request/<int:follow_request_id>/',RejectFollowView.as_view(),name =' rejectfollowView'),
-    path('unfollow/<int:id>/',UnfollowView.as_view(),name =' unfollowView'),
+    path('unfollow/<int:follow_record_id>/',UnfollowView.as_view(),name =' unfollowView'),
     path('get-follower/',GetFollowersView.as_view(),name =' getfollowerView'),
     path("following/",FollowingView.as_view(),name='following'),
     path("story/",CreateStory.as_view(),name='createstory'),
